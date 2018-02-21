c997eec94febb9be7e487f52374091ce8b8667dd
Aryan Suri <aryanrsuri@gmail.com>
2018-02-20 17:54:49 -0800

    fixed RobotMap errors (mainly DIO recourse )

--- src/org/usfirst/frc295/GrizzlynatorBase/RobotMap.java
+++ src/org/usfirst/frc295/GrizzlynatorBase/RobotMap.java
@@ -39,26 +39,29 @@ public class RobotMap
 	// SPECIFY WHICH ROBOT WE ARE RUNNING
 	public static final RobotID ROBOT_ID = RobotID.BOT_COMP1;
 	
-	public static final short PCM_ELEVATOR_SOL_FOLLOW =7;
-	public static final short PCM_ELEVATOR_SOL = 0;
+	public static final short PCM_ELEVATOR_SOL_FOLLOW = 7;
+	public static final short PCM_ELEVATOR_SOL        = 0;
 	
 	public static final short CAN_PCM_MODULE = 0;
 	public static final short CAN_PCM_PORT_DRIVE_SHIFTER = 1;
-	public static final short CAN_ESC_DRIVE_LEFT_FRONT = 3;
-	public static final short CAN_ESC_DRIVE_LEFT_BACK = 4;
-	public static final short CAN_ESC_DRIVE_RIGHT_FRONT = 5;
-	public static final short CAN_ESC_DRIVE_RIGHT_BACK = 6;
+	public static final short CAN_ESC_DRIVE_LEFT_FRONT   = 3;
+	public static final short CAN_ESC_DRIVE_LEFT_BACK    = 4;
+	public static final short CAN_ESC_DRIVE_RIGHT_FRONT  = 5;
+	public static final short CAN_ESC_DRIVE_RIGHT_BACK   = 6;
 
-	public static final short PWM_ESC_LEFT_INTAKE_MOTOR = 1;
+	public static final short PWM_ESC_LEFT_INTAKE_MOTOR  = 1;
 	public static final short PWM_ESC_RIGHT_INTAKE_MOTOR = 2;
-	public static final short PWM_ELEVATOR_SPARK = 0;
-	public static final short RLY_GREENLIGHT = 0;
-	public static final short RLY_FLASHLIGHT = 1;
+	public static final short PWM_ELEVATOR_SPARK         = 0;
+	
+	public static final short RLY_GREENLIGHT             = 0;
+	public static final short RLY_FLASHLIGHT             = 1;
 
 	public static final short DIO_ENC_DRIVE_LEFT_CHAN1 = 0;
 	public static final short DIO_ENC_DRIVE_LEFT_CHAN2 = 1;
 	public static final short DIO_ENC_DRIVE_RIGHT_CHAN1= 2;
 	public static final short DIO_ENC_DRIVE_RIGHT_CHAN2= 3;
+	public static final short DIO_ENC_ELEVATOR_CHAN1   = 4;
+	public static final short DIO_ENC_ELEVATOR_CHAN2   = 5;
 	public static final short DIO_ELEVATOR_SCALE_LIMIT=  6;
 	public static final short DIO_ELEVATOR_VAULT_LIMIT=  7;
 	public static final short DIO_ELEVATOR_SWITCH_LIMIT= 8;
@@ -70,7 +73,10 @@ public class RobotMap
 	public static final double DRIVE_KI = 0.00;
 	public static final double DRIVE_KD = 0.00;
 	public static final double DRIVE_KF = 0.00;
-
+	public static final double RISE = 0.7;
+	public static final double LOWER = -0.1;
+	public static final double ZERO = 0.0;
+	
 	// LOOPER PARAMETERS
 	public static final double LOOPER_PERIOD_HZ = 50;
 	public static final short TBD = 0;
--- src/org/usfirst/frc295/GrizzlynatorBase/subsystems/SysElevator.java
+++ src/org/usfirst/frc295/GrizzlynatorBase/subsystems/SysElevator.java
@@ -68,7 +68,7 @@ public class SysElevator extends Subsystem
 		SwitchlimitSwitchSwitch = new DigitalInput(RobotMap.DIO_ELEVATOR_SWITCH_LIMIT);
 		SwitchlimitSwitchBottom = new DigitalInput(RobotMap.DIO_ELEVATOR_BOTTOM_LIMIT);
 		
-		_encoElevator = new Encoder(RobotMap.DIO_ENC_ELEVATOR_LEFT_CHAN1, RobotMap.DIO_ENC_ELEVATOR_RIGHT_CHAN2, false, EncodingType.k4X);
+		_encoElevator = new Encoder(RobotMap.DIO_ENC_ELEVATOR_CHAN1, RobotMap.DIO_ENC_ELEVATOR_CHAN2, false, EncodingType.k4X);
 		_encoElevator.setDistancePerPulse(1.0);
 		_encoElevator.setPIDSourceType(PIDSourceType.kDisplacement);
 	
