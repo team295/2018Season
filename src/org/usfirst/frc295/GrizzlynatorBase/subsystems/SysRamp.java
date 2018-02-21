aae67d2f3afb70433764d55047323201e7bb7ad7
redsnake25@gmail.com <redsnake25@gmail.com>
2018-02-20 20:06:04 -0800

    fixed classpath(TalonSRX was missing)

--- src/org/usfirst/frc295/GrizzlynatorBase/subsystems/SysRamp.java
+++ src/org/usfirst/frc295/GrizzlynatorBase/subsystems/SysRamp.java
@@ -15,35 +15,7 @@ public class SysRamp extends Subsystem{
 	private DoubleSolenoid PushoutMain = new DoubleSolenoid(RobotMap.PCM_TOP_SOL, RobotMap.PCM_TOP_SOL_FOLLOW);
 	private final DoubleSolenoid.Value RETRACT_SOLENOID = DoubleSolenoid.Value.kReverse;
 	private final DoubleSolenoid.Value EXTEND_SOLENOID = DoubleSolenoid.Value.kForward;
-<<<<<<< HEAD
-=======
 
-	private Servo RRamp = new Servo(1);
-	private Servo LRamp = new Servo(2);
-
-//	private DoubleSolenoid RDS = new DoubleSolenoid(5,6);
-//	private DoubleSolenoid LDS = new DoubleSolenoid(7,8);
-
-	private DoubleSolenoid RDS;
-	private DoubleSolenoid LDS;
-	public boolean hasOpened(short pin) {
-		if (pin == 1) {
-			if(RRamp.getAngle() > 270) {
-				return true;
-			} else {
-				return false;
-			}
-		} else if (pin == 2) {
-			if(LRamp.getAngle() > 270) {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
-	}
->>>>>>> Added joystick interception for TOP and drive straight, still need to tweak drive straight correction
 	
 	
 	public void OpenRamp(){
